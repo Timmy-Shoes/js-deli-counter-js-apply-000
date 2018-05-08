c07f94f1b695ab0452f0e4073b5735588bd89a9d
schoonmt@gmail.com <schoonmt@gmail.com>
2018-05-08 08:13:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeli = [];
 function takeANumber(katzDeliLine, name) {
-  return ("Welcome, " + name + ". You are number" + katzDeliLine.length + " in line.")
+  return ("Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.")
 }
 function testTakeANumber() {
   katzDeli = ["Ada", "Grace", "Kent"]
@@ -10,7 +10,7 @@ return expectedResponse === response && katzDeli.length === 3;
 }
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-    return "There is nobody waiting to be served";
+    return "There is nobody waiting to be served!";
   } else {
     return 'Currently serving ' + katzDeliLine.shift() + "."
   }
