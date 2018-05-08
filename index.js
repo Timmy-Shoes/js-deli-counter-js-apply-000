1610eea2f8eb297f0f64e1909aea3a81a0c038be
schoonmt@gmail.com <schoonmt@gmail.com>
2018-05-08 08:22:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ var katzDeli = ["Ada", "Grace", "Kent"];
 function takeANumber(katzDeliLine, name) {
   return ("Welcome, " + name + ". You are number " + katzDeliLine.push(name) + " in line.")
 }
-function testTakeANumber() {
-  katzDeli = ["Ada", "Grace", "Kent"]
-var response = takeANumber(katzDeli, "Matz")
-var expectedResponse = "Welcome, Matz. You are number 3 in line."
-return expectedResponse === response && katzDeli.length === 3;
-}
+
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
@@ -15,12 +10,7 @@ function nowServing(katzDeliLine) {
     return 'Currently serving ' + katzDeliLine.shift() + "."
   }
 }
-function testNowServingWithPeopleOnLine() {
-  katzDeli = ["Ada", "Grace", "Kent"];
-  var response = nowServing(katzDeli);
-  var expectedResponse = "Currently Serving Ada"
-  return response === expectedResponse;
-}
+
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
